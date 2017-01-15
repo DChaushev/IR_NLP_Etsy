ac9d29db718b163079f28beca0026f5c4573ae2f
Dimitar Chaushev <d.chaushev073@gmail.com>
2017-01-15 22:16:32 +0200

    Initial commit. Project setup.

---
+++ EtsyClassifier/src/main/java/com/ir/etsy/classifier/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ir.etsy.classifier;
+
+/**
+ *
+ * @author Dimitar
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("I am the Classifier's main class!");
+    }
+}
