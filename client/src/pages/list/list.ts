232250a2f59225860c84d0983ea114a920ab27b7
lolotobg <loloto.loloto@gmail.com>
2017-01-21 18:32:47 +0200

    Clean up template ionic app

--- client/src/pages/list/list.ts
+++ client/src/pages/list/list.ts
@@ -11,18 +11,13 @@ import { ItemDetailsPage } from '../item-details/item-details';
 })
 export class ListPage {
   icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  items: Array<{listing_id: number}>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
-
     this.items = [];
     for(let i = 1; i < 11; i++) {
       this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+        listing_id: i*123 + i*5 + i%13 + i,
       });
     }
   }
