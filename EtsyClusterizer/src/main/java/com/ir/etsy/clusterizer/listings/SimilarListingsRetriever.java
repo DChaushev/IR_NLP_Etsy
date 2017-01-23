7bc1238d4d4c98cf6c17e9b43ad8ccd9227c63a0
Dimitar Chaushev <d.chaushev073@gmail.com>
2017-01-23 22:34:22 +0200

    REST service implemented. TODO: implement SimilarListingsRetriever.

---
+++ EtsyClusterizer/src/main/java/com/ir/etsy/clusterizer/listings/SimilarListingsRetriever.java
@@ -0,0 +1,21 @@
+package com.ir.etsy.clusterizer.listings;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ *
+ * @author Dimitar
+ */
+public class SimilarListingsRetriever {
+
+    //NOTE: change it to lead to your index.
+    private static final String INDEX_DIR = "D:\\IR_project_etsy_files\\index";
+
+    public static List<Listing> process(Listing listing) {
+        //TODO: implement
+        return Arrays.asList(listing);
+    }
+
+}
--- EtsyClusterizer/src/main/java/com/ir/etsy/clusterizer/services/EtsyResource.java
+++ EtsyClusterizer/src/main/java/com/ir/etsy/clusterizer/services/EtsyResource.java
@@ -1,9 +1,17 @@
 package com.ir.etsy.clusterizer.services;
 
-import javax.ws.rs.GET;
+import com.ir.etsy.clusterizer.listings.Listing;
+import com.ir.etsy.clusterizer.listings.SimilarListingsRetriever;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.codehaus.jackson.map.ObjectMapper;
 
 /**
  *
@@ -12,10 +20,22 @@ import javax.ws.rs.core.MediaType;
 @Path("get-similar-listings")
 public class EtsyResource {
 
-    //TODO: imlement
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getSimilarListings() {
-        return "HALLO!";
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getSimilarListings(String json) {
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            Listing listing = mapper.readValue(json, Listing.class);
+            List<Listing> similarListings = SimilarListingsRetriever.process(listing);
+
+            String jsonResult = mapper.writeValueAsString(similarListings);
+
+            return Response.ok().entity(jsonResult).build();
+        } catch (IOException ex) {
+            Logger.getLogger(EtsyResource.class.getName()).log(Level.SEVERE, null, ex);
+            return Response.status(Response.Status.BAD_REQUEST).entity(
+                    "The send json is of unsupported format!\n" + ex.getMessage()).build();
+        }
     }
 }
